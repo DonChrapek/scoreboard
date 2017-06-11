bf1ce72c3212cbc698521932ead76cdc2e36d394
DonChrapek <daniel.chrapczynski@gmail.com>
2017-06-11 18:46:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <h1>Here comes the Scoreboard</h1>
+    );
+  }
+}
