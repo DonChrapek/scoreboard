0d11797dda1855007b53bc126bf28412b0176d99
DonChrapek <daniel.chrapczynski@gmail.com>
2017-06-11 20:41:47 +0200

    Made incrementScore and decrementScore methods

--- src/Player.js
+++ src/Player.js
@@ -2,16 +2,28 @@ import React from 'react';
 import './css/player.css';
 
 export default class Player extends React.Component {
+    
+    constructor() {
+        super();
+        this.state = {
+            score: 0
+        }
+    }
+    
+    incrementScore() { this.setState({ score: this.state.score + 1 }) }
+    
+    decrementScore() { this.setState({ score: this.state.score - 1 }) } 
+    
     render() {
         return (
             <div className="player-wrapper">
                 <div className="player-name">
-                    <h3>Name</h3>
+                    <h3>{this.props.name}</h3>
                 </div>
                 <div className="player-score">
-                    <button id="decrement" className="red-button"> - </button>
-                    <div id="score-box">21</div>
-                    <button id="increment" className="green-button"> + </button>
+                    <button id="decrement" className="red-button" onClick={this.decrementScore.bind(this)}> - </button>
+                    <div id="score-box">{this.state.score}</div>
+                    <button id="increment" className="green-button" onClick={this.incrementScore.bind(this)}> + </button>
                 </div>
             </div>
         )
--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -4,16 +4,36 @@ import './css/scoreboard.css';
 import Player from './Player';
 
 export default class Scoreboard extends React.Component {
+    
+    PLAYERS = []
+    
+    constructor() {
+        super();
+        this.state = { 
+            players: this.PLAYERS,
+        }
+    }
+    
+    addPlayer(e) {
+        e.preventDefault();
+        
+        let newPlayerName = document.getElementById('newplayer').value;
+        newPlayerName === "" ? alert("Player's name can't be blank") : this.setState({ players: this.state.players.concat(newPlayerName) });
+        document.getElementById('newplayer').value = "";
+    }
+    
     render() {
         return (
             <section id="scoreboard">
                 <div className="scoreboard-title">
                     <h1>Scoreboard</h1>
+                    <input type="text" name="newplayer" id="newplayer" />
+                    <input type="submit" value="+ Add player" onClick={this.addPlayer.bind(this)} />
                 </div>
                 <div className="scoreboard-rank">
-                    <Player />
-                    <Player />
-                    <Player />
+                    { this.state.players.map((name, index) => {
+                        return <Player name={name} key={index} />
+                    }) }
                 </div>
             </section>
         )
