14f8b6f78a05cdcb72c220a353ff4fad6818586d
DonChrapek <daniel.chrapczynski@gmail.com>
2017-06-11 18:58:15 +0200

    Created footer
    Created as stateless component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import './css/App.css';
 
-export default class App extends Component {
+import {Footer} from './Footer';
+
+export default class App extends React.Component {
   render() {
     return (
-      <h1>Here comes the Scoreboard</h1>
+      <div>
+          <h1>Here comes the Scoreboard</h1>
+          <Footer />
+      </div>
     );
   }
 }
---
+++ src/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './css/footer.css';
+
+export const Footer = () => {
+    return (
+        <p>Copyright &copy;Daniel</p>
+    );
+}
